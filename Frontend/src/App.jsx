f934995ee6389a1fc676de62d3fe8cfe7ce1344b
Kanishk mittal <kanishkmittal2005@gmail.com>
2024-10-06 11:46:22 +0530

    added router in frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import './App.css'
+import Login from './pages/Login'
+import SellerDashboard from './pages/SellerDashboard'
+import UserDashboard from './pages/UserDashboard'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 function App() {
-  
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Login />
+    },
+    {
+      path: '/seller/:id',
+      element: <SellerDashboard />
+    },
+    {
+      path: '/user/:id',
+      element: <UserDashboard />
+    }
+  ])
+
   return (
     <>
-      <h1>Scamazon</h1>
-      <p>Please Wait this site is still under progress</p>
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ Frontend/src/pages/SellerDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SellerDashboard = ({sellerId}) => {
+    return (
+        <div>
+            This is seller dashboard
+        </div>
+    )
+}
+
+export default SellerDashboard
