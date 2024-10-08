65f74f159005013cd2f9ed3aeb6b38f3bcb91712
Roshan Binoj <roshan23bcs9@iiitkottayam.ac.in>
2024-10-08 11:45:44 +0530

    Initial template of Login added

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        '5xl': '0 35px 60px -15px rgba(0, 0, 0, 0.5)', // Customize the shadow here
+      },
+    },
   },
   plugins: [],
 }
